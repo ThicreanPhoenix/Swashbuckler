081e35bfb3adb3aab15d2a1a29ca24ec62a4317a
thicreanphoenix <thicreanphoenix@gmail.com>
2025-10-25 13:47:07 -0500

    Edited Minor code

--- Swashbuckler.cs
+++ Swashbuckler.cs
@@ -2044,7 +2044,7 @@ public class AddSwash
         ModManager.AddFeat(LeadingDance);
         ModManager.AddFeat(SwaggeringInitiative);
         ModManager.AddFeat(TwinParry);
-        ModManager.AddFeat(CombinationFinisher);
+        //ModManager.AddFeat(CombinationFinisher);
         ReplaceOpportunityAttack();
         ModManager.AddFeat(PreciseFinisher);
         ModManager.AddFeat(BleedingFinisher);
